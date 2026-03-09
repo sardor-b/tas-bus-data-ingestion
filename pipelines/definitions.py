39ab4324c919c89b32e8fc4921ad14c63693f1aa
sardor-b <153438122+sardor-b@users.noreply.github.com>
2026-03-09 13:38:55 +0500

    changes

--- pipelines/definitions.py
+++ pipelines/definitions.py
@@ -1,5 +1,5 @@
 from dagster import Definitions
-from jobs.ingest_bus_routes import bus_routes_job, bus_routes_schedule
+from pipelines.jobs.ingest_bus_routes import bus_routes_job, bus_routes_schedule
 
 defs = Definitions(
     jobs=[bus_routes_job],
