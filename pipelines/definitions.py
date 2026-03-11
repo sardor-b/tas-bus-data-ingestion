1e6d20946111890bc7e116cd44768ab997a4600a
sardor-b <153438122+sardor-b@users.noreply.github.com>
2026-03-11 11:36:06 +0500

    Added systemd timer and scheduler

--- pipelines/definitions.py
+++ pipelines/definitions.py
@@ -3,18 +3,13 @@ from pipelines.jobs.ingest_bus_job import bus_job, bus_schedule
 from pipelines.jobs.ingest_bus_routes import bus_routes_job, bus_routes_schedule
 from pipelines.jobs.ingest_bus_stations import bus_stations_job, bus_stations_schedule
 from pipelines.jobs.calculate_bus_movement import bus_movement_job, bus_movement_schedule
-from pipelines.sensors.ingest_bus_gps_updates import gps_streaming_sensor, gps_ingestion_job
 
 defs = Definitions(
     jobs=[
         bus_routes_job,
         bus_stations_job,
-        gps_ingestion_job,
         bus_job,
         bus_movement_job
-    ],
-    sensors=[
-        gps_streaming_sensor
     ],
     schedules=[
         bus_routes_schedule,
