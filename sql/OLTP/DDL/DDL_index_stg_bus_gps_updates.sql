f11102f799da1ab12e5fe863195775f17adc4c15
sardor-b <153438122+sardor-b@users.noreply.github.com>
2026-03-11 12:02:29 +0500

    changes

--- sql/OLTP/DDL/DDL_index_stg_bus_gps_updates.sql
+++ sql/OLTP/DDL/DDL_index_stg_bus_gps_updates.sql
@@ -1,5 +1,2 @@
-CREATE INDEX idx_bus_gps_source
-    ON stg.bus_gps_updates (source, update_dt DESC);
-
-CREATE INDEX idx_bus_gps_payload
-    ON stg.bus_gps_updates USING GIN (object_value jsonb_path_ops);
+CREATE INDEX idx_bus_gps_updates_update_dt
+    ON stg.bus_gps_updates (update_dt DESC);
--- sql/OLTP/DDL/DDL_table_stg_bus_gps_updates.sql
+++ sql/OLTP/DDL/DDL_table_stg_bus_gps_updates.sql
@@ -1,8 +1,6 @@
-CREATE TABLE stg.bus_gps_updates (
+CREATE UNLOGGED TABLE stg.bus_gps_updates (
     id           uuid        DEFAULT uuidv7(),
     source       varchar     NOT NULL,
     object_value jsonb       NOT NULL,
-    update_dt    timestamptz NOT NULL DEFAULT now(),
-    PRIMARY KEY (id, update_dt)
+    update_dt    timestamptz NOT NULL DEFAULT now()
 );
-
