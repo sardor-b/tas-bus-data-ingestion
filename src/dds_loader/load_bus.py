993cd75faacdc10a6850ead657082acf668047e1
sardor-b <153438122+sardor-b@users.noreply.github.com>
2026-03-13 01:10:15 +0500

    changes

--- src/dds_loader/load_bus.py
+++ src/dds_loader/load_bus.py
@@ -257,68 +257,59 @@ async def s_bus_movement(
                 FROM stg.bus_gps_updates
             ),
             latest AS (
-                SELECT DISTINCT ON (hk_bus)
+                SELECT
                     hk_bus,
-                    hash_diff
+                    ping_dt
                 FROM dds.s_bus_movement
-                ORDER BY hk_bus, load_dt DESC
             ),
             bus_movement AS (
                 SELECT DISTINCT ON (bus_id, ping_dt)
-                    v #>> '{bus, id}'  AS bus_id,
+                    v #>> '{bus, id}'      AS bus_id,
                     st_setsrid(
                         st_makepoint(
-                            (v #>> '{bus, ly}')::float,  -- longitude first
-                            (v #>> '{bus, lx}')::float   -- latitude second
+                            (v #>> '{bus, ly}')::float,
+                            (v #>> '{bus, lx}')::float
                         ), 4326
-                    )                  AS location,
-                    v #>> '{bus, speed}' AS speed,
-                    v #>> '{course}'     AS course,
-                    v #>> '{status}'     AS status,
+                    )                      AS location,
+                    v #>> '{bus, speed}'   AS speed,
+                    v #>> '{course}'       AS course,
+                    v #>> '{status}'       AS status,
                     CASE
                         WHEN (v #>> '{qDirection}')::int =  1 THEN 'origin'
                         WHEN (v #>> '{qDirection}')::int =  0 THEN 'destination'
                         WHEN (v #>> '{qDirection}')::int = -1 THEN 'None'
-                    END                  AS direction,
-                    update_dt            AS ping_dt
+                    END                    AS direction,
+                    update_dt              AS ping_dt
                 FROM bus_gps_updates
+                ORDER BY bus_id, ping_dt
             )
             SELECT
-                md5(bus_id)          AS hk_bus,
-                location,
-                speed::numeric(5,1),
-                course::int,
-                status,
-                direction,
-                ping_dt,
-                NOW()                AS load_dt,
-                'stg.bus_gps_updates' AS load_source,
+                md5(bm.bus_id)            AS hk_bus,
+                bm.location,
+                bm.speed::numeric(5,1),
+                bm.course::int,
+                bm.status,
+                bm.direction,
+                bm.ping_dt,
+                NOW()                  AS load_dt,
+                'stg.bus_gps_updates'  AS load_source,
                 md5(
                     concat_ws(
                         '||',
-                        st_astext(location),
-                        speed,
-                        course,
-                        status,
-                        direction,
-                        ping_dt
+                        st_astext(bm.location),
+                        bm.speed,
+                        bm.course,
+                        bm.status,
+                        bm.direction,
+                        bm.ping_dt
                     )
-                )                    AS hash_diff
-            FROM bus_movement
-            LEFT JOIN latest l ON l.hk_bus = md5(bus_id)
-            WHERE md5(
-                concat_ws(
-                    '||',
-                    st_astext(location),
-                    speed,
-                    course,
-                    status,
-                    direction,
-                    ping_dt
-                )
-            ) IS DISTINCT FROM l.hash_diff
-            ON CONFLICT (hk_bus, ping_dt)
-                DO NOTHING;
+                )                      AS hash_diff
+            FROM bus_movement bm
+            LEFT JOIN latest l
+                ON l.hk_bus = md5(bus_id)
+                AND l.ping_dt = bm.ping_dt
+            WHERE l.hk_bus IS NULL
+            ;
         """,
         params={}
     )
